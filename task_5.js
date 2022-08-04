2acc90422ae171427247b8b2a71755427fb31948
Sergey <92508690+SN737@users.noreply.github.com>
2022-08-04 17:26:37 +0300

    Update task_5.js

--- task_5.js
+++ task_5.js
@@ -1,15 +1,31 @@
 
-class device {
-    electric: true,
-    heating: true,
-    isOn: function (time) {
-        let consumption = this.power*time;
+class Device {
+
+    constructor (deviceName, power) {
+        this.name = deviceName;
+        this.power = power;
+        this.electric = 'true';
+        this.heating = 'true';
+    }
+    
+    isOn (time) {
+        let consumption = this.power*(time/3600);
         console.log (`Потреблено ${consumption} Вт, за  ${time} секунд`);
-        
-    },
-    isOff:function(time) {
+    }
+    isOff (time) {
         let consumption = this.power*time*0;
         console.log (`Потреблено ${consumption} Вт, за  ${time} секунд.`);
         console.log (`устройство ${this.name} выключено и не потребляет энергию.`);
     }
-};
+}
+
+
+// const Device.kitchen {
+
+// }
+
+let ndevice = new Device('lamp', 50);
+
+console.log(ndevice);
+ndevice.isOn(100)
+
